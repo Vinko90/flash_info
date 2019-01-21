614afd33c5609fd6146d1145f7ae135489e7e5b9
WarOfDevil <vnktux@protonmail.com>
2019-01-21 16:55:11 +0100

    Test working release

--- include/ifd.h
+++ include/ifd.h
@@ -8,17 +8,42 @@
 #define MAX_REGIONS 9
 #define MAX_REGIONS_OLD 5
 
+typedef struct {
+	uint32_t flreg[MAX_REGIONS];
+} __attribute__((packed)) frba_t;
 
-/*enum ifd_version {
-	IFD_VERSION_1,
-	IFD_VERSION_2,
-};*/
+// flash descriptor
+typedef struct {
+	uint32_t flvalsig;
+	uint32_t flmap0;
+	uint32_t flmap1;
+	uint32_t flmap2;
+} __attribute__((packed)) fdbar_t;
 
 struct region_name {
 	const char *long_name;
 	const char *short_name;
 };
 
+typedef struct {
+	int base, limit, size;
+} region_t;
+
 void open_rom_to_memory(char* filename);
 
-#endif 
+void dump_layout();
+
+frba_t *find_frba();
+
+fdbar_t *find_flash_descriptor();
+
+void dump_frba_layout(const frba_t *frba);
+
+region_t get_region(const frba_t *frba, unsigned int region_type);
+
+void dump_region_layout(unsigned int num, const frba_t *frba);
+
+const char *region_name_short(unsigned int region_type);
+
+
+#endif
--- src/ifd.cpp
+++ src/ifd.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
+#include <stdio.h>
 #include <fcntl.h>
-//#include <fstream>
 #include <unistd.h>
-//#include <sys/types.h>
 #include <sys/stat.h>
 #include "ifd.h"
 
 using namespace std;
+
+/**
+ * PTR_IN_RANGE - examine whether a pointer falls in [base, base + limit)
+ * @param ptr:    the non-void* pointer to a single arbitrary-sized object.
+ * @param base:   base address represented with char* type.
+ * @param limit:  upper limit of the legal address.
+ *
+ */
+#define PTR_IN_RANGE(ptr, base, limit)			\
+	((const char *)(ptr) >= (base) &&		\
+	 (const char *)&(ptr)[1] <= (base) + (limit))
 
 /*static void convert_bytes(int size)
 {
@@ -18,10 +25,7 @@ using namespace std;
     cout << "Mb " << size/1024/1024 << endl;
 }*/
 
-
-
-
-/*static const struct region_name region_names[MAX_REGIONS] = {
+static const struct region_name region_names[MAX_REGIONS] = {
 	{ "Flash Descriptor", "fd"},
 	{ "BIOS", 			"bios"}, 
 	{ "Intel ME", 		  "me"},
@@ -32,7 +36,7 @@ using namespace std;
 	{ "Reserved_3",     "res3"}, 
 	{ "EC",               "ec"}, 
 };
-*/
+
 static char *bios_image;
 static int  bios_size = -1;
 
@@ -71,21 +75,114 @@ void open_rom_to_memory(char* filename)
 
 	close(file_descriptor);
 
-	//TBD
-    //check_ifd_version();
+	
+    dump_layout();
 
     delete[] bios_image;
 }
 
-static void check_ifd_version(char *image, int size)
+void dump_layout()
 {
-	if (is_platform_ifd_2())
-		ifd_version = IFD_VERSION_2;
-	else
-		ifd_version = get_ifd_version_from_fcba(image, size);
+	const frba_t *frba = find_frba();
+	if (!frba)
+		exit(EXIT_FAILURE);
 
-	if (ifd_version == IFD_VERSION_1)
-		max_regions = MAX_REGIONS_OLD;
-	else
-		max_regions = MAX_REGIONS;
+	dump_frba_layout(frba);
+}
+
+frba_t *find_frba()
+{
+	fdbar_t *fdb = find_flash_descriptor();
+	if (!fdb)
+		return NULL;
+	frba_t *frba =
+		(frba_t *) (bios_image + (((fdb->flmap0 >> 16) & 0xff) << 4));
+	return PTR_IN_RANGE(frba, bios_image, bios_size) ? frba : NULL;
+}
+
+fdbar_t *find_flash_descriptor()
+{
+	int i, found = 0;
+
+	/* Scan for FD signature */
+	for (i = 0; i < (bios_size - 4); i += 4) {
+		if (*(uint32_t *) (bios_image + i) == 0x0FF0A55A) {
+			found = 1;
+			break;	// signature found.
+		}
+	}
+
+	if (!found) {
+		printf("No Flash Descriptor found in this image\n");
+		return NULL;
+	}
+
+	fdbar_t *fdb = (fdbar_t *) (bios_image + i);
+	return PTR_IN_RANGE(fdb, bios_image, bios_size) ? fdb : NULL;
+}
+
+void dump_frba_layout(const frba_t *frba)
+{
+	unsigned int i;
+
+	//Open the new file
+
+	for (i = 0; i < MAX_REGIONS; i++) {
+		region_t region = get_region(frba, i);
+		/* is region invalid? */
+		if (region.size < 1)
+			continue;
+
+		cout << "------------" << region.base << endl;
+
+		dump_region_layout(i, frba);
+		//Write single region to file
+
+
+
+	}
+	//Close file
+	cout << "Wrote layout done" << endl;
+}
+
+region_t get_region(const frba_t *frba, unsigned int region_type)
+{
+	int base_mask = 0x7fff; //0xfff  for IFD v1
+	int limit_mask;
+	uint32_t flreg;
+	region_t region;
+
+	limit_mask = base_mask << 16;
+
+	if (region_type >= MAX_REGIONS) {
+		cout << "Invalid region type: " << region_type << endl;
+		exit (EXIT_FAILURE);
+	}
+
+	flreg = frba->flreg[region_type];
+	region.base = (flreg & base_mask) << 12;
+	region.limit = ((flreg & limit_mask) >> 4) | 0xfff;
+	region.size = region.limit - region.base + 1;
+
+	if (region.size < 0)
+		region.size = 0;
+
+	return region;
+}
+
+void dump_region_layout(unsigned int num, const frba_t *frba)
+{
+	region_t region = get_region(frba, num);
+
+	printf("%08x:%08x %s\n", region.base, region.limit, region_name_short(num));
+}
+
+const char *region_name_short(unsigned int region_type)
+{
+	if (region_type >= MAX_REGIONS) {
+		cout << "Invalid region type" << endl;
+		exit (EXIT_FAILURE);
+	}
+
+	return region_names[region_type].short_name;
 }
